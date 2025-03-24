4bbaef4d7f2362a5cd2288f30ce3f814eac48a6e
javatest <tillebaev2@gmail.com>
2025-03-24 22:44:47 +0600

    hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,20 +1,49 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/example_schema";
+    private static final String URL = "jdbc:mysql://localhost:3306/my_db";
     private static final String USER = "root";
     private static final String PASSWORD = "machoman";
 
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
     public static Connection getConnection() {
-        Connection connection = null;
         try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
-            System.out.println("Ошибка подключения к базе данных");
-            e.printStackTrace();
+            throw new RuntimeException("Ошибка подключения к базе данных", e);
         }
-        return connection;
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", URL);
+            configuration.setProperty("hibernate.connection.username", USER);
+            configuration.setProperty("hibernate.connection.password", PASSWORD);
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.addAnnotatedClass(jm.task.core.jdbc.model.User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            return configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
