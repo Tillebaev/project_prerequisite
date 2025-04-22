2ba094ec4b23af520085e0b959a7425c6d2b75f1
javatest <tillebaev2@gmail.com>
2025-04-22 11:09:47 +0600

    task_2_2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorldOne = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorldOne.getMessage());
+        HelloWorld helloWorldTwo = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorldTwo.getMessage());
+
+        Cat catOne = (Cat) applicationContext.getBean("cat");
+        System.out.println(catOne.getMessage());
+        Cat catTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println(catTwo.getMessage());
+
+        System.out.println("HelloWorld одинаковые? " + (helloWorldOne == helloWorldTwo));
+        System.out.println("Cat одинаковые? " + (catOne == catTwo));
+
     }
 }
